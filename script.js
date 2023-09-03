799ce664db5e78855d010dff4b7acaf2846485d8
Iamneo <techadmin@iamneo.ai>
2023-09-03 14:52:22 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const
+const cells=document.querySelectorAll(".cell");
+const board=document.getElementById("board");
+const winnerText=document.getElementById("winner-text");
+const resetButton=document.getElementById("reset-button");
+let currentPlayer="X";
+let gammeOver=false;
+
+function checkWinner(){
+    const winnigCombos=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for(const combo of winnigCombos){
+        const[a,b,c]=combo
+    }
+}
